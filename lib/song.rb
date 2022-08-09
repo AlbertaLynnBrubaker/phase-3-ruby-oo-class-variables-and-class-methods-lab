2ecb37556ed5cbc83a25d8bd5950628b668f74b6
Alberta Lynn Brubaker <findingalberta@gmail.com>
2022-08-08 21:16:34 -0500

    commit tree 3aa61167392a5e66fe2480afc8e41742e69bd0a0

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+require "pry"
+
+class Song
+
+  @@artists = []
+  @@genres = []
+  @@count = 0
+
+  attr_accessor :name, :artist, :genre
+  
+  def initialize (name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists.push(self.artist)
+    @@genres.push(self.genre)
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    @@genres.tally
+  end
+
+  def self.artist_count
+    @@artists.tally
+  end
+end
